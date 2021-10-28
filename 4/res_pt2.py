6fc831dabc056a01c65c351192bb42580cbefb88
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-10-28 18:14:52 +0100

    stupid but 4p2 works

--- 4/res_pt2.py
+++ 4/res_pt2.py
@@ -4,6 +4,7 @@ import sys
 fields = set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
 
 def check_field(field):
+    tipo = field[:3]
     value = field[4:]
     if re.search("byr:[0-9]{4}", field) and 1920 <= int(value) <= 2002:
         return "byr"
@@ -15,12 +16,26 @@ def check_field(field):
         return "hgt"
     if re.search("hgt:[0-9]{2}in", field) and 59 <= int(value[:-2]) <= 76:
         return "hgt"
-    if re.search("hcl:#([0-9]|[a-f]){6}", field):
-        return "hcl"
-    if re.search("ecl:(amb|blu|brn|gry|grn|hzl|oth)", field):
+    if tipo == "hcl":
+        test = 1
+        if value[0] != "#":
+            test = 0
+        for el in value[1:]:
+            if el not in set(["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]):
+                test = 0
+        if test == 1:
+            return "hcl"
+    if tipo == "ecl" and value in set(["amb","blu","brn","gry","grn","hzl","oth"]):
         return "ecl"
-    if re.search("pid:[0-9]{9}", field):
-        return "pid"
+    if tipo == "pid":
+        test = 1
+        if len(value) != 9:
+            test = 0
+        for el in value:
+            if el not in set(["0","1","2","3","4","5","6","7","8","9"]):
+                test = 0
+        if test == 1:
+            return "pid"
     return False
 
 with open("input", "r") as file:
@@ -44,7 +59,6 @@ for passport in txt:
         tmp = check_field(el.rstrip("\n")) 
         if tmp != False:
             check.add(tmp)
-    # print(passport)
 
 # print(fields, check)
 if fields == check:
