922805f4bf06cd857182ababa85587ea761e7f3e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-07 21:51:01 +0000

    day06 complete

--- 2021/day06/res.py
+++ 2021/day06/res.py
@@ -3,9 +3,11 @@ with open('input', 'r') as file:
 
 lanternfish = list(map(int, filter(lambda x: x != ',', lanternfish)))
 
-lanternfish = [3,4,3,1,2]
+# lanternfish = [3,4,3,1,2]
 
 # print(lanternfish)
+
+# old
 
 # def nextnumber(x):
 #     if x > 6:
@@ -16,7 +18,7 @@ lanternfish = [3,4,3,1,2]
 # length = 0
 # for el in lanternfish:
 #     tmp = [el]
-#     for day in range(256):
+#     for day in range(80):
 #         count = tmp.count(0)
 #         tmp = list(map(nextnumber, tmp))
 #         for i in range(count):
@@ -26,51 +28,28 @@ lanternfish = [3,4,3,1,2]
 
 # print(length)
 
-count = 0
-def generator(timer, day, total_days):
-	print(f"generator(8, {day}, {total_days})")
-	print((total_days - day - timer)//7 + 1)
-	if day+timer >= total_days:
-		return 0
-	ret = (total_days - day - timer)//7 + 1
-	for i in range(day+timer+1, total_days, 7):
-		ret += generator(8, i, total_days)
+# new
+
+def generator(timer, day, total_days, table):
+	if day > total_days:
+		return 0 
+	ret = 1
+	for i in range(day+timer+1, total_days+1, 7):
+		if table[i] == -1:
+			table[i] = generator(8, i, total_days, table)
+		ret += table[i]
 	return ret
 
-# count = 0
-# for el in lanternfish:
-# 	count += generator(el, 0, 18) + 1
-# 	print(generator(el, 0, 18))
+def getAnswer(max_days):
+	table = [-1 for _ in range(max_days + 1)]
+	for i in reversed(range(len(table))):
+		table[i] = generator(8, i, max_days, table)
 
-print(generator(4, 0, 18))
+	count = 0
+	for el in lanternfish:
+		count += generator(el, 0, max_days, table)
 
-# print(count)
+	return count
 
-# 4
-# 4
-# 4 -> 3
-# 6
-# 5
-# 28
-
-# 5 + 5 + 5 + 7
-
-#  0: 3 4 3 1 2
-#  1: 2 3 2 0 1
-#  2: 1 2 1 6 0 8
-#  3: 0 1 0 5 6 7 8
-#  4: 6 0 6 4 5 6 7 8 8
-#  5: 5 6 5 3 4 5 6 7 7 8
-#  6: 4 5 4 2 3 4 5 6 6 7
-#  7: 3 4 3 1 2 3 4 5 5 6
-#  8: 2 3 2 0 1 2 3 4 4 5
-#  9: 1 2 1 6 0 1 2 3 3 4 8
-# 10: 0 1 0 5 6 0 1 2 2 3 7 8
-# 11: 6 0 6 4 5 6 0 1 1 2 6 7 8 8 8
-# 12: 5 6 5 3 4 5 6 0 0 1 5 6 7 7 7 8 8
-# 13: 4 5 4 2 3 4 5 6 6 0 4 5 6 6 6 7 7 8 8
-# 14: 3 4 3 1 2 3 4 5 5 6 3 4 5 5 5 6 6 7 7 8
-# 15: 2 3 2 0 1 2 3 4 4 5 2 3 4 4 4 5 5 6 6 7
-# 16: 1 2 1 6 0 1 2 3 3 4 1 2 3 3 3 4 4 5 5 6 8
-# 17: 0 1 0 5 6 0 1 2 2 3 0 1 2 2 2 3 3 4 4 5 7 8
-# 18: 6 0 6 4 5 6 0 1 1 2 6 0 1 1 1 2 2 3 3 4 6 7 8 8 8 8
+print(getAnswer(80))
+print(getAnswer(256))
