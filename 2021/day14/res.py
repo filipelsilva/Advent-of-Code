1326837463b554842403b949b216e488ef8923ca
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-12-14 17:38:02 +0000

    working on part 2

--- 2021/day14/res.py
+++ 2021/day14/res.py
@@ -6,24 +6,34 @@ with open('input', 'r') as file:
 # print(template)
 # print(connections)
 
-# template = "NNCB"
-# connections = [["CH", "B"],["HH", "N"],["CB", "H"],["NH", "C"],["HB", "C"],["HC", "B"],["HN", "C"],["NN", "C"],["BH", "H"],["NC", "B"],["NB", "B"],["BN", "B"],["BB", "N"],["BC", "B"],["CC", "N"],["CN", "C"]]
+template = "NNCB"
+connections = [["CH", "B"],["HH", "N"],["CB", "H"],["NH", "C"],["HB", "C"],["HC", "B"],["HN", "C"],["NN", "C"],["BH", "H"],["NC", "B"],["NB", "B"],["BN", "B"],["BB", "N"],["BC", "B"],["CC", "N"],["CN", "C"]]
 
-match = list(map(lambda x: x[0], connections))
+conns = dict((value[0], value[0][0] + value[1] + value[0][1]) for value in connections)
+# print(conns)
 
-print(template)
+def getIter(step, template):
+    next_template = template[0]
+    for i in range(step, len(template)):
+        # print(template[i-step:i+1], step+1)
+        if template[i-step:i+1] in conns:
+            next_template += conns[template[i-step:i+1]][1:]
+        else:
+            next_template += getIter(step-1, template[i-step:i+1])
+    return next_template
 
-for step in range(40):
-    new_template = template[0]
-    for i in range(1, len(template)):
-        for j in range(len(match)):
-            # print(f"{template[i-1:i+1]} == {match[i]}")
-            if template[i-1:i+1] == match[j]:
-                new_template += connections[j][1]
-        new_template += template[i]
-        # print(new_template)
-    template = new_template
-    # print(template)
+for step in range(2):
+    # print(step+1)
+    tmp = dict()
+    for el in conns:
+        tmp.update({conns[el]: getIter(step+1, conns[el])})
+    conns.update(tmp)
+    print(conns)
 
-count = dict((value, template.count(value)) for value in template)
-print(max(count.values()) - min(count.values()))
+# print(max([len(el) for el in conns]))
+# final = getIter(max([len(el) for el in conns] + [len(template)]), template)
+final = getIter(4, template)
+print(final)
+
+# count = dict((value, template.count(value)) for value in template)
+# print(max(count.values()) - min(count.values()))
