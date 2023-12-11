9a81704706561c4a65658e483c6671a6a0cce85f
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-12-11 21:19:08 +0000

    day3 done for 2023

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -4,35 +4,38 @@ use std::{collections::HashMap, fs};
 fn main() {
     let input = fs::read_to_string("input").unwrap();
     let re_numbers = Regex::new(r"\d+").unwrap();
-    let re_symbols = Regex::new(r"[#%&*+/=@$%]").unwrap();
+    let re_symbols = Regex::new(r"[#%&*+/\-=@$%]").unwrap();
 
     let mut numbers = HashMap::new();
     input.lines().enumerate().for_each(|(i, line)| {
         re_numbers.find_iter(line).for_each(|capture| {
             let mut possible_positions = vec![];
             for ele in capture.start()..capture.end() {
-                possible_positions.push((i, ele));
+                possible_positions.push((i as isize, ele as isize));
             }
             numbers.insert(
                 possible_positions,
-                (capture.as_str().parse::<i32>().unwrap(), false),
+                (capture.as_str().parse::<u32>().unwrap(), false),
             );
         });
     });
 
     let mut part1: u64 = 0;
+    let mut part2: u64 = 0;
     input.lines().enumerate().for_each(|(i, line)| {
         re_symbols.find_iter(line).for_each(|capture| {
-            let possible_positions = vec![
-                (i - 1, capture.start() - 1),
-                (i - 1, capture.start()),
-                (i - 1, capture.start() + 1),
-                (i, capture.start() - 1),
-                (i, capture.start() + 1),
-                (i + 1, capture.start() - 1),
-                (i + 1, capture.start()),
-                (i + 1, capture.start() + 1),
+            // println!("{:?}", (i, capture));
+            let possible_positions: Vec<(isize, isize)> = vec![
+                (i as isize - 1, capture.start() as isize - 1),
+                (i as isize - 1, capture.start() as isize),
+                (i as isize - 1, capture.start() as isize + 1),
+                (i as isize, capture.start() as isize - 1),
+                (i as isize, capture.start() as isize + 1),
+                (i as isize + 1, capture.start() as isize - 1),
+                (i as isize + 1, capture.start() as isize),
+                (i as isize + 1, capture.start() as isize + 1),
             ];
+            let mut part_numbers: Vec<u64> = vec![];
             for (i, j) in possible_positions {
                 numbers
                     .iter_mut()
@@ -40,16 +43,22 @@ fn main() {
                         return key.contains(&(i, j));
                     })
                     .for_each(|(_, val)| {
-                        println!("{:?}", val);
                         if !val.1 {
                             val.1 = true;
                             part1 += val.0 as u64;
+                            // println!("{:?}", (val, part1));
+                            if capture.as_str() == "*" {
+                                part_numbers.push(val.0 as u64);
+                            }
                         }
                     });
+            }
+            if capture.as_str() == "*" && part_numbers.len() == 2 {
+                part2 += part_numbers.iter().product::<u64>();
             }
         });
     });
 
     println!("Part 1: {:?}", part1);
-    // println!("Part 2: {:?}", part2);
+    println!("Part 2: {:?}", part2);
 }
