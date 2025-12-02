2e368236ceba13ee61eb7633f00eba903b12e3bb
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-02 13:06:21 +0000

    part 1 done

--- 2025/day01/src/main.rs
+++ 2025/day01/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input2").unwrap();
+    let input = fs::read_to_string("input").unwrap();
 
     let mut pointer = 50;
 
@@ -10,9 +10,10 @@ fn main() {
 
     input.lines().for_each(|line| {
         let (direction, distance) = line.split_at(1);
-        let number_distance = distance.parse::<i32>().unwrap();
+        let mut number_distance = distance.parse::<i32>().unwrap();
 
         zero_counter_pt2 += number_distance / 100;
+        number_distance %= 100;
 
         let new_pointer = (pointer
             + match direction {
@@ -20,23 +21,30 @@ fn main() {
                 "L" => -number_distance,
                 _ => panic!("should catch R or L"),
             })
-            % 100;
-
-        if pointer < 0 && new_pointer >= 0 {
-            zero_counter_pt2 += 1;
-        } else if pointer > 0 && new_pointer <= 0 {
-            zero_counter_pt2 += 1;
-        } else if new_pointer == 0 {
-            zero_counter_pt2 += 1;
-        }
+        .rem_euclid(100);
 
         if new_pointer == 0 {
             zero_counter += 1;
+            zero_counter_pt2 += 1;
+        } else if pointer != 0 {
+            let dist = (new_pointer - pointer).abs();
+
+            if dist != number_distance {
+                zero_counter_pt2 += 1;
+            }
+
+            if dist == 50 && pointer != 0 {
+                if new_pointer < pointer && direction == "R" {
+                    zero_counter_pt2 += 1;
+                } else if new_pointer > pointer && direction == "L" {
+                    zero_counter_pt2 += 1;
+                }
+            }
         }
 
         println!("{pointer} -> {new_pointer} | {line} | {zero_counter} | {zero_counter_pt2}");
 
-        pointer = new_pointer.rem_euclid(100);
+        pointer = new_pointer;
     });
 
     println!("Part1: {zero_counter}");
