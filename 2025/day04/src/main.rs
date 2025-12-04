1de56f014d5b9dfdfe91585ab1ba083c65eb42d8
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-04 10:37:33 +0000

    day04 done

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     let binding = fs::read_to_string("input").unwrap();
-    let input = binding
+    let mut input = binding
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
@@ -48,6 +48,53 @@ fn main() {
                     println!("FOUND {y},{x}")
                 }
             }
+        }
+    }
+
+    loop {
+        let last_counter = counter_pt2;
+
+        for y in 0..input.len() {
+            for x in 0..input[0].len() {
+                if input[y][x] == '@' {
+                    let mut counter = 0;
+
+                    let start_y = match y {
+                        0 => 0,
+                        _ => y - 1,
+                    };
+                    let start_x = match x {
+                        0 => 0,
+                        _ => x - 1,
+                    };
+
+                    let end_y = if y == input.len() - 1 { y } else { y + 1 };
+                    let end_x = if x == input[y].len() - 1 { x } else { x + 1 };
+
+                    for i in start_y..=end_y {
+                        for j in start_x..=end_x {
+                            if i >= input.len() && j >= input[y].len() {
+                                continue;
+                            }
+                            if i == y && j == x {
+                                continue;
+                            }
+                            if input[i][j] == '@' {
+                                counter += 1;
+                            }
+                        }
+                    }
+
+                    if counter < 4 {
+                        counter_pt2 += 1;
+                        input[y][x] = 'x';
+                    }
+                }
+            }
+        }
+
+        if counter_pt2 - last_counter == 0 {
+            break;
         }
     }
 
