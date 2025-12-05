d02da91004cec1ea2f712c98a1ca450373a79369
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-05 09:24:13 +0000

    part2 done. had some help from AI :/
    such a pain in the ass to debug

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -33,50 +33,25 @@ fn main() {
         }
     }
 
-    let mut final_ranges: Vec<RangeInclusive<u64>> = vec![];
-    loop {
-        for range_to_add in &ranges {
-            if final_ranges.is_empty() {
-                final_ranges.push(range_to_add.clone());
-                continue;
+    let mut final_ranges: Vec<RangeInclusive<u64>> = Vec::new();
+
+    ranges.sort_by_key(|r| *r.start());
+    for range in ranges {
+        if let Some(last) = final_ranges.last_mut() {
+            if *range.start() <= *last.end() + 1 {
+                let new_end = std::cmp::max(*last.end(), *range.end());
+                let new_start = *last.start();
+                *last = new_start..=new_end;
+            } else {
+                final_ranges.push(range);
             }
-
-            let mut should_add = true;
-
-            for i in 0..final_ranges.len() {
-                if final_ranges[i].contains(range_to_add.start()) {
-                    if !final_ranges[i].contains(range_to_add.end()) {
-                        final_ranges[i] = *range_to_add.start()..=*range_to_add.end()
-                    }
-                    should_add = false;
-                } else if final_ranges[i].contains(range_to_add.end()) {
-                    if !final_ranges[i].contains(range_to_add.start()) {
-                        final_ranges[i] = *range_to_add.start()..=*range_to_add.end()
-                    }
-                    should_add = false;
-                }
-
-                if !should_add {
-                    break;
-                }
-            }
-
-            if should_add {
-                final_ranges.push(range_to_add.clone());
-            }
+        } else {
+            final_ranges.push(range);
         }
-
-        if ranges.len() == final_ranges.len() {
-            break;
-        }
-
-        ranges = final_ranges.clone();
-        final_ranges.clear();
     }
 
-    println!("{final_ranges:#?}");
-    for range in final_ranges {
-        counter_pt2 += range.count();
+    for range in &final_ranges {
+        counter_pt2 += range.clone().count();
     }
 
     println!("Part1: {counter_pt1}");
