6c771795f63defb478708dd9ee760f5d8df1780c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-05 08:13:11 +0000

    attempt at part2

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     let split = input.trim().split("\n\n").collect::<Vec<_>>();
 
-    let ranges = split[0]
+    let mut ranges = split[0]
         .lines()
         .map(|el| {
             let range = el
@@ -31,6 +31,63 @@ fn main() {
                 break;
             }
         }
+    }
+
+    let mut final_ranges: Vec<(u64, u64)> = vec![];
+    loop {
+        for range_to_add in &ranges {
+            if final_ranges.is_empty() {
+                final_ranges.push(*range_to_add);
+                continue;
+            }
+
+            let mut should_add = true;
+            for final_range in &mut final_ranges {
+                if range_to_add.0 == final_range.0 && range_to_add.1 == final_range.1 {
+                    should_add = false;
+                } else if range_to_add.0 == range_to_add.1
+                    && (range_to_add.0 == final_range.0 || range_to_add.0 == final_range.1)
+                {
+                    should_add = false;
+                } else if range_to_add.0 < final_range.0 && range_to_add.1 > final_range.1 {
+                    should_add = false;
+                    final_range.0 = range_to_add.0;
+                    final_range.1 = range_to_add.1;
+                } else if range_to_add.0 >= final_range.0
+                    && range_to_add.0 <= final_range.1
+                    && range_to_add.1 >= final_range.1
+                {
+                    should_add = false;
+                    final_range.1 = range_to_add.1;
+                } else if range_to_add.1 <= final_range.1
+                    && range_to_add.1 >= final_range.0
+                    && range_to_add.0 <= final_range.0
+                {
+                    should_add = false;
+                    final_range.0 = range_to_add.0;
+                }
+
+                if should_add == false {
+                    break;
+                }
+            }
+
+            if should_add {
+                final_ranges.push(*range_to_add);
+            }
+        }
+
+        if ranges.len() == final_ranges.len() {
+            break;
+        }
+
+        ranges = final_ranges.clone();
+        final_ranges.clear();
+    }
+
+    println!("{final_ranges:#?}");
+    for range in final_ranges {
+        counter_pt2 += (range.1 - range.0) + 1;
     }
 
     println!("Part1: {counter_pt1}");
